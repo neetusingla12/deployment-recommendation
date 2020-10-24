aac505d66465e889a9bfb7b0eadff94fced2af18
neetusingla12(Neetu Singla) <64238599+neetusingla12@users.noreply.github.com>
2020-10-24 17:04:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -16,20 +16,19 @@ def home():
     return render_template('index.html',prediction_text="News recommended are:", model)
 
 
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+   # int_features = [int(x) for x in request.form.values()]
+    #final_features = [np.array(int_features)]
+    #prediction = model.predict(final_features)
 
-#@app.route('/predict',methods=['POST'])
-#def predict():
-#    '''
-#    For rendering results on HTML GUI
-#    '''
-#   # int_features = [int(x) for x in request.form.values()]
-#    #final_features = [np.array(int_features)]
-#    #prediction = model.predict(final_features)
-#
-#    #output = round(prediction[0], 2)
-#    output = model
-#
-#    return render_template('index.html', prediction_text="News recommended are:", output)
+    #output = round(prediction[0], 2)
+    output = model
+
+    return render_template('index.html', prediction_text="News recommended are:", model)
 
 
 
